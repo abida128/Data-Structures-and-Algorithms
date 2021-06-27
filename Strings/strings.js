14e0c181da8ce7ce1aa1652756fcb4a189c7b98a
unknown <abida.nizam11@gmail.com>
2021-06-27 13:22:29 +0500

    Practice Basics of String

---
+++ Strings/strings.js
@@ -0,0 +1,16 @@
+let name = "abida nizam";
+
+// strings are subscriptable or indexable
+
+console.log(name[0]);
+
+// string
+console.log([name.length-4])
+
+//index 4
+console.log(name[name.length[4]])
+
+// we can run loops on string 
+for(let i =0; i<name.length; i++){
+    console.log(name[i], 'string characters');
+}
